4092e86f1b49bb06b47b9cb44adf96aee1c756cf
walkperro <johnsalsberry1980@gmail.com>
2025-12-25 20:16:27 -0600

    fix: remove stripe apiVersion to satisfy sdk types

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -1,6 +1,4 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // If this ever causes a build error, remove this line and redeploy.
-  apiVersion: "2025-11-17.clover",
-});
+  // If this ever causes a build error, remove this line and redeploy.});
